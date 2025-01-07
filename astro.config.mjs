90029dc9b40e315d82e5562f5ba3e32ab28dcc53
Gary Varner <garysarahvarner@gmail.com>
2025-01-06 20:21:46 -0500

    fixing astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ghostylabz.com",
+  site: "https://www.ghostylabz.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
